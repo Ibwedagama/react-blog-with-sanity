0dec5f27cdfcf70077f445e635433ef0edbd3d47
Ida Bagus Wedagama <wardana.wedagama@gmail.com>
2021-02-26 08:49:08 +0800

    [IBW] add project schema to admin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const Home = () => {
-	return <div>Hello from Home</div>
+	return (
+		<main className='absolute w-full h-screen'>
+			<img
+				src='/assets/3DVisaCard.jpg'
+				alt='Image of Card'
+				className='absolute object-cover w-full h-screen'
+			/>
+			<section className='relative flex justify-center min-h-screen pt-12 lg:pt-16'>
+				<h1 className='text-6xl text-white font-bold cursive leading-none lg:leading-snug'>
+					Hello, I'm Ibwedagama
+				</h1>
+			</section>
+		</main>
+	)
 }
 
 export default Home
